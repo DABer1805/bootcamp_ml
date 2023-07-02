185941e401a0408ca01d055c3462ef80a4406976
DABer1805 <138240716+DABer1805@users.noreply.github.com>
2023-07-02 03:09:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -51,17 +51,16 @@ def predict_status(features: np.ndarray, comment_text: str, author_name: str):
     """ Предсказать оценку отзыва и отрисовать комментарий пользователя """
     # Загружаем скейлер и модель
     scaler = joblib.load('scaler.pkl')
-    st.write(dir(pickle))
-    # with open('model.pickle', 'rb') as f:
-    #     model = pickle.load(f)
+    with open('model.pickle', 'rb') as f:
+        model = pickle.load(f)
 
     # Отрисовываем комментарий пользователя
-    #write_comment(
-    #    Comment(
-    #        model.predict(scaler.transform(features)).round().astype(int),
-    #        comment_text, author_name
-    #    )
-    #)
+    write_comment(
+        Comment(
+            model.predict(scaler.transform(features)).round().astype(int),
+            comment_text, author_name
+        )
+    )
 
 
 def start() -> None:
